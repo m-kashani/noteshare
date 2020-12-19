eed0bd0d9d4492cfbcb369aa3245a835b036d8a3
wizif <rezagina68@gmail.com>
2020-12-19 04:36:13 +0330

    changed for testing dbs
    index function get json from user and add json directly to dbs
    update function just return all dbs document

--- api/src/Core/general/general.py
+++ api/src/Core/general/general.py
@@ -1,4 +1,9 @@
-def index(data):
-    return data
-def update(data):
+
+def index(data,col):
+    x=col.insert_one(data)
+    return x.inserted_id
+def update(data,col):
+    for x in col.find():
+        col.delete_one({"_id":x["_id"]})
+        print(x)
     return {"status":"you are updating"}
