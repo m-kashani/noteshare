21ce75d6d68a0f61e7ee041ba2c315c07739df73
wizif <rezagina68@gmail.com>
2020-12-22 05:55:08 +0330

    added 2 action

--- api/src/Core/routes.py
+++ api/src/Core/routes.py
@@ -1,10 +1,13 @@
 from src.Core.general.general import index
+from src.Core.general.general import clear
 from src.Core.general.general import update
-
+from src.Core.general.general import find
 def dispacther():
 
     dispatch={
         "index":index,
-        "update":update
+        "clear":clear,
+        "update":update,
+        "find":find
     }
     return dispatch
