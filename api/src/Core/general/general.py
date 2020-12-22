8e445ab0da360517c05f3694f91c62f938414af8
wizif <rezagina68@gmail.com>
2020-12-22 05:54:51 +0330

    add new function
    now we have one update select clear for all the database this is a feature...write once and control all the dbs.i think we got problem with this methods.this is general file and we got more general functions.we need to make models for profile and books editing
    becuase there we need to parse token and these mothods never use there for security issues

--- api/src/Core/general/general.py
+++ api/src/Core/general/general.py
@@ -3,6 +3,21 @@ def index(data,col):
     x=col.insert_one(data)
     return x.inserted_id
 def update(data,col):
+    myquery = {"_id":data['_id']}
+    newvalues = { "$set": data }
+    col.update_one(myquery,newvalues)
+    _ret=[]
+    for x in col.find():
+        _ret.append(x)
+    return _ret
+def find(data,col):
+    myquery=data
+    limit=data.pop('limit')
+    _ret=[]
+    for x in col.find(data).limit(limit):
+        _ret.append(x)
+    return _ret
+def clear(data,col):
     for x in col.find():
         col.delete_one({"_id":x["_id"]})
         print(x)
